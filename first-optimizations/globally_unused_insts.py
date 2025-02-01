8f6bcdb75bf882a4a00433b44626d7a740051fcf
Neel Patel <neelpatelbiz@gmail.com>
2025-02-01 10:37:55 -0500

    modify json_loaded program

--- first-optimizations/globally_unused_insts.py
+++ first-optimizations/globally_unused_insts.py
@@ -4,6 +4,7 @@ import sys
 used = set()
 passed_func = []
 deleted = True
+num_deleted = 0
 
 def pass0(prog):
   global used
@@ -18,29 +19,27 @@ def pass0(prog):
 
 def pass1(prog):
   global deleted, passed_func
+  global num_deleted
   for inst in prog:
 
     if 'dest' in inst:
       if inst['dest'] not in used:
         # get identifier of the inst to remove
         deleted = True
+        num_deleted += 1
         continue
     passed_func.append(inst)
 
 prog = json.load(sys.stdin)
-passed_prog = {'functions': []}
 for func in prog['functions']:
   deleted = True
   while deleted:
-    pass0(func['instrs'])
+    deleted = False # if deleted gets set to False before the condition is checked, then the pass can only run once
+    pass0(func['instrs']) # after each pass, we need to update the func
     pass1(func['instrs'])
+    func['instrs'] = passed_func
+    passed_func = []
     used.clear()
-    deleted = False
-  passed_prog['functions'].append({'name': func['name'], 'instrs': passed_func})
-  if 'type' in func:
-    passed_prog['functions'][-1]['type'] = func['type']
-  if 'args' in func:
-    passed_prog['functions'][-1]['args'] = func['args']
-  passed_func = []
 
-print(json.dumps(passed_prog, indent=2))
+
+print(json.dumps(prog, indent=2))
