30593206109f77298826f9828034e3c9af552095
Neel Patel <neelpatelbiz@gmail.com>
2025-02-03 00:53:22 -0500

    make ids share the same canonical name as the value they are sourcing, unless there is no entry in the table, then make one

--- first-optimizations/lvn.py
+++ first-optimizations/lvn.py
@@ -27,7 +27,7 @@ def form_bbs(function):
   return (bbs, num_bbs)
 
 expr_num_map = OrderedDict()
-
+var2num = {}
 def get_table_repr(expr):
   global expr_num_map, val_ctr
   # Const
@@ -42,18 +42,14 @@ def get_table_repr(expr):
     if expr['op'] in ('add', 'sub', 'mul', 'div', 'lt', 'eq', 'gt', 'ge', 'le', 'and', 'or'):
       return (expr['op'], get_table_repr(expr['args'][0]), get_table_repr(expr['args'][0]))
     if expr['op'] in ('id'):
-      if (expr['op'], expr['args'][0]) in expr_num_map:
-        return (True, list(expr_num_map.keys()).index(expr))
+      if expr['args'][0] in var2num:
+        var2num[expr['dest']] = var2num[expr['args'][0]]
+        return (True, var2num[expr['args'][0]])
       else:
-        expr_num_map[(expr['op'], expr['args'][0])] = expr['dest']
-        return (False, (expr['op'], expr['args'][0]))
-    if expr['op'] in ('const'):
-      if (expr['op'], expr['type'], expr['value']) in expr_num_map:
-        return (True, list(expr_num_map.keys()).index(expr))
-      else:
-        expr_num_map[(expr['op'], expr['type'], expr['value'])] = expr['dest']
-        return (False, (expr['op'], expr['type'], expr['value']))
+        var2num[expr['dest']] = len(expr_num_map)
+        expr_num_map[(expr['op'], expr['args'][0])] = expr['args'][0]
 
+        return (False, (expr['op'], expr['args'][0]))
     if expr['op'] in ('jmp'):
       return (expr['op'], expr)
     if expr['op'] in ('print'):
@@ -64,10 +60,15 @@ def get_table_repr(expr):
   if 'label' in expr:
     return (expr['label'], expr)
 
-def new_instr(expr):
-  if(expr[0] == 'id'):
-    source = expr_num_map[expr[1]]
-    return {'args': []}
+def new_instr(old_inst, expr):
+  if(expr[0] == False):
+    new_inst = old_inst
+  else:
+    if old_inst['op'] in ('const'):
+      # this inst can be deleted
+      return None
+    if old_inst['op'] in ('id'):
+      mapped_expr = list(expr_num_map.keys())[expr[1]]
 
 
 prog = json.load(sys.stdin)
@@ -76,6 +77,7 @@ for function in prog['functions']:
   bbs = bbinfo[0]
   for bb in bbs:
     expr_num_map.clear()
+    new_bb = []
     for instr in bb:
       print(f'Instr: {instr}')
       subst_expr = get_table_repr(instr)
