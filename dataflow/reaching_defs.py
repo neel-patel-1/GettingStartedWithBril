b1006a6dce9741f91245c1c506020eb7cd08fd17
Neel Patel <neelpatelbiz@gmail.com>
2025-02-07 19:59:11 -0500

    create a mapping from bbs to predecessors

--- dataflow/reaching_defs.py
+++ dataflow/reaching_defs.py
@@ -23,7 +23,7 @@ def form_bbs(function):
       if instr['op'] in TERMINATORS:
         if bb_label == None:
           bb_label = get_fresh_bb_name()
-        bbs.append((bb, bb_label))
+        bbs.append((bb, bb_label,None,None))
         bb = []
         num_bbs += 1
         bb_label = None
@@ -31,18 +31,36 @@ def form_bbs(function):
       if bb_label == None:
         bb_label = get_fresh_bb_name()
       bb = bb[:-1]
-      bbs.append((bb, bb_label))
+      bbs.append((bb, bb_label,None,None))
       bb_label = instr['label']
       num_bbs += 1
   if bb:
     if bb_label == None:
       bb_label = get_fresh_bb_name()
-    bbs.append((bb, bb_label))
+    bbs.append((bb, bb_label,None,None))
     num_bbs += 1
   return (bbs)
 
+def form_predecessor_map(bbs):
+  pred_map = {}
+  for index, bb in enumerate(bbs):
+    if 'op' in bb[0][-1]:
+      if bb[0][-1]['op'] in TERMINATORS:
+        for label in bb[0][-1]['labels']:
+          if label in pred_map:
+            pred_map[label].append(index)
+          else:
+            pred_map[label] = [index]
+      elif index < len(bbs) - 1:
+        if bbs[index + 1][1] in pred_map:
+          pred_map[bbs[index + 1][1]].append(index)
+        else:
+          pred_map[bbs[index + 1][1]] = [index]
+  return pred_map
 
 prog = json.load(sys.stdin)
 for function in prog['functions']:
   bbs = form_bbs(function)
-  print(bbs)
+  pred_map = form_predecessor_map(bbs)
+
+print(pred_map )
