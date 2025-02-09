796e29f654f88f0950adc5fbabaef5fd5e12fc3f
Neel Patel <neelpatelbiz@gmail.com>
2025-02-08 20:37:05 -0500

    set the bb names

--- dataflow/reaching_defs.py
+++ dataflow/reaching_defs.py
@@ -5,6 +5,8 @@ import queue
 TERMINATORS = 'br', 'jmp', 'ret'
 use_defs = {}
 outsets = {}
+pred_map = {}
+succ_map = {}
 
 counter = 0
 def get_fresh_bb_name():
@@ -21,6 +23,8 @@ def form_bbs(function):
   bb_label = 'Entry'
   for instr in instrs:
     bb.append(instr)
+    if 'label' in instr and len(bb) == 1:
+      bb_label = instr['label']
     if 'op' in instr:
       if instr['op'] in TERMINATORS:
         if bb_label == None:
@@ -44,7 +48,7 @@ def form_bbs(function):
   return (bbs)
 
 def form_predecessor_map(bbs):
-  pred_map = {}
+  global pred_map
   for index, bb in enumerate(bbs):
     if 'op' in bb[0][-1]:
       if bb[0][-1]['op'] in TERMINATORS:
@@ -58,10 +62,28 @@ def form_predecessor_map(bbs):
           pred_map[bbs[index + 1][1]].append(index)
         else:
           pred_map[bbs[index + 1][1]] = [index]
-  return pred_map
+  print(f'pred_map: {pred_map}')
+
+
+def form_successor_map(bbs):
+  global succ_map
+  for index, bb in enumerate(bbs):
+    if 'op' in bb[0][-1]:
+      if bb[0][-1]['op'] in TERMINATORS:
+        for label in bb[0][-1]['labels']:
+          if index in succ_map:
+            succ_map[index].append(label)
+          else:
+            succ_map[index] = [label]
+      elif index < len(bbs) - 1:
+        if index in succ_map:
+          succ_map[index].append(bbs[index + 1][1])
+        else:
+          succ_map[index] = [bbs[index + 1][1]]
+
 
 def get_repr(index,bb):
-  bb_instnum = bb[1] + str(index)
+  bb_instnum = bb[1] + "_" + str(index)
   return bb_instnum
 
 def transfer(bb, inset):
@@ -74,11 +96,15 @@ def transfer(bb, inset):
       outset[inst['dest']] = get_repr(index,bb)
   return outset
 
-def create_inset(bb):
+def create_inset(bb,bb_list):
+  global pred_map
   inset = dict()
   if bb[1] in pred_map:
     for pred in pred_map[bb[1]]:
-      inset = {**inset, **outsets[pred]}
+      if bb_list[pred][1] in outsets:
+        for key in outsets[bb_list[pred][1]]:
+          inset[key] = outsets[bb_list[pred][1]][key]
+    print(f'bb: {bb[1]} inset: {inset}')
   return inset
 
 
@@ -86,8 +112,11 @@ prog = json.load(sys.stdin)
 for function in prog['functions']:
   outsets.clear()
   use_defs.clear()
+  pred_map.clear()
+  succ_map.clear()
   bbs = form_bbs(function)
-  pred_map = form_predecessor_map(bbs)
+  form_predecessor_map(bbs)
+  form_successor_map(bbs)
 
 bbq = queue.Queue()
 for bb in bbs:
@@ -95,7 +124,7 @@ for bb in bbs:
 
 while not bbq.empty():
   bb = bbq.get()
-  inset = create_inset(bb)
+  inset = create_inset(bb,bbs)
   if bb[1] in outsets:
     outset = outsets[bb[1]]
   else:
@@ -103,5 +132,10 @@ while not bbq.empty():
   new_outset = transfer(bb, inset)
   if new_outset != outset:
     outsets[bb[1]] = new_outset
-    bbq.put(bb[1])
-    print(new_outset)
+    if bb[1] in succ_map:
+      for succ in succ_map[bb[1]]:
+        bbq.put(bbs[succ])
+    print(f'bb: {bb[1]} outset: {new_outset}')
+
+for use in use_defs:
+  print(f"{use}: {use_defs[use]}")
