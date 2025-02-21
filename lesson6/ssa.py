2e00c75ba2d801ddc75b6279016839cc42abb2a6
Neel Patel <neelpatelbiz@gmail.com>
2025-02-21 18:31:40 -0500

    cfg && frontier

--- lesson6/ssa.py
+++ lesson6/ssa.py
@@ -1,10 +1,103 @@
 import json
 import sys
+import queue
 
 TERMINATORS = 'br', 'jmp', 'ret'
 defs = {}
+pred_map = {}
+succ_map = {}
+dom_map = {}
+dom_map_changed = True
 
 counter = 0
+
+class TreeNode:
+    def __init__(self, value):
+        self.value = value
+        self.children = []
+
+    def add_child(self, child_node):
+        self.children.append(child_node)
+
+class Tree:
+    def __init__(self, root_value):
+        self.root = TreeNode(root_value)
+
+    def add_node(self, value, parent_value):
+        parent_node = self.find_node(self.root, parent_value)
+        if parent_node:
+            parent_node.add_child(TreeNode(value))
+
+    def find_node(self, current_node, value):
+        if current_node.value == value:
+            return current_node
+        for child in current_node.children:
+            found_node = self.find_node(child, value)
+            if found_node:
+                return found_node
+        return None
+
+    def traverse(self, node=None):
+        if node is None:
+            node = self.root
+        print(node.value)
+        for child in node.children:
+            self.traverse(child)
+
+    def display(self, node=None, level=0):
+        if node is None:
+            node = self.root
+        print(' ' * level + str(node.value), file=sys.stderr)
+        for child in node.children:
+            self.display(child, level + 2)
+
+def form_predecessor_map(bbs):
+  global pred_map
+  for index, bb in enumerate(bbs):
+    # print(f'bb: {bb[1]}', file=sys.stderr)
+    if 'op' in bb[0][-1]:
+      if bb[0][-1]['op'] in TERMINATORS:
+        if 'labels' in bb[0][-1]:
+          for label in bb[0][-1]['labels']:
+            if label in pred_map:
+              pred_map[label].append(index)
+            else:
+              pred_map[label] = [index]
+      elif index < len(bbs) - 1:
+        if bbs[index + 1][1] in pred_map:
+          pred_map[bbs[index + 1][1]].append(index)
+        else:
+          pred_map[bbs[index + 1][1]] = [index]
+    else: # empty bb
+      if index < len(bbs) - 1:
+        if bbs[index + 1][1] in pred_map:
+          pred_map[bbs[index + 1][1]].append(index)
+        else:
+          pred_map[bbs[index + 1][1]] = [index]
+
+def form_successor_map(bbs):
+  global succ_map
+  for index, bb in enumerate(bbs):
+    if 'op' in bb[0][-1]:
+      if bb[0][-1]['op'] in TERMINATORS:
+        if 'labels' in bb[0][-1]:
+          for label in bb[0][-1]['labels']:
+            if index in succ_map:
+              succ_map[index].append(label)
+            else:
+              succ_map[index] = [label]
+      elif index < len(bbs) - 1:
+        if index in succ_map:
+          succ_map[index].append(bbs[index + 1][1])
+        else:
+          succ_map[index] = [bbs[index + 1][1]]
+    else: # empty bb
+      if index < len(bbs) - 1:
+        if index in succ_map:
+          succ_map[index].append(bbs[index + 1][1])
+        else:
+          succ_map[index] = [bbs[index + 1][1]]
+
 def get_fresh_bb_name():
   global counter
   counter += 1
@@ -31,7 +124,7 @@ def form_bbs(function):
         bb_label = None
     if 'label' in instr and len(bb) > 1:
       if bb_label == None:
-        print(f'bb: {bb}', file=sys.stderr)
+        # print(f'bb: {bb}', file=sys.stderr)
         bb_label = get_fresh_bb_name()
       bb = bb[:-1]
       bbs.append((bb, bb_label,None,None))
@@ -45,11 +138,88 @@ def form_bbs(function):
     num_bbs += 1
   return (bbs)
 
+def initialize_bb_doms(bbs):
+  global dom_map
+  dom_map[bbs[0][1]] = set({bbs[0][1]})
+  for bb in bbs[1:]:
+    dom_map[bb[1]] = set(bb[1] for bb in bbs)
+
+
+def gen_cfg(bbs):
+  global pred_map
+  global succ_map
+  cfg = Tree(bbs[0][1])
+  for index, bb in enumerate(bbs):
+    if index in succ_map:
+      # print(f'Adding {bb[1]} to cfg', file=sys.stderr)
+      for b in succ_map[index]:
+        cfg.add_node(b, bb[1])
+  return cfg
+
+
+def dom_frontier(bb, tree):
+  global dom_map
+  global succ_map
+  dom_frontier = set()
+  # search down the tree in a breadth-first manner stopping threads that see the same node again -> {child_set}
+  child_set = set()
+  q = queue.Queue()
+  q.put(bb[1])
+  while not q.empty():
+    node = q.get()
+    if tree.find_node(tree.root, node) == None:
+      continue
+    for child in tree.find_node(tree.root, node).children:
+      if child.value not in child_set:
+        child_set.add(child.value)
+        q.put(child.value)
+
+  # for each child in child_set, if bb is not in the dom_map of child, add child to dom_frontier
+  for child in child_set:
+    if bb[1] not in dom_map[child]:
+      dom_frontier.add(child)
+  return dom_frontier
+
+def get_bb_doms(bb, bb_list):
+  global dom_map
+  global pred_map
+  # print(f'Getting doms for {bb[1]}', file=sys.stderr)
+  # print(f'pred_map: {pred_map}', file=sys.stderr)
+  if bb[1] in pred_map:
+    preds = pred_map[bb[1]]
+    doms = set(bb[1] for bb in bb_list)
+    for pred in preds:
+      if bb_list[pred][1] in dom_map:
+        # print(f'Intersecting Pred Doms from {bb_list[pred][1]}: {dom_map[bb_list[pred][1]]}', file=sys.stderr)
+        doms.intersection_update(dom_map[bb_list[pred][1]])
+    doms.add(bb[1])
+    return doms
+  else:
+    return dom_map[bb[1]]
+
+def gen_d_tree(bbs):
+  global dom_map
+  global succ_map
+  dtree = Tree(bbs[0][1])
+  for index, bb in enumerate(bbs):
+    if index in succ_map:
+      for b in succ_map[index]:
+        if bb[1] in dom_map[b]:
+          dtree.add_node(b, bb[1])
+  return dtree
+
 prog = json.load(sys.stdin)
 for function in prog['functions']:
   vars = set()
-  defs = {}
+  dom_map_changed = True
+  dom_map.clear()
+  pred_map.clear()
+  succ_map.clear()
+
   bbs = form_bbs(function)
+  form_predecessor_map(bbs)
+  form_successor_map(bbs)
+
   for bb in bbs:
     for inst in bb[0]:
       if 'dest' in inst:
@@ -57,5 +227,29 @@ for function in prog['functions']:
         if inst['dest'] not in defs:
           defs[inst['dest']] = []
         defs[inst['dest']].append(bb[1])
-  print(f'vars: {vars}', file=sys.stderr)
-  print(f'defs: {defs}', file=sys.stderr)
+  # print(f'vars: {vars}', file=sys.stderr)
+  # print(f'defs: {defs}', file=sys.stderr)
+
+  initialize_bb_doms(bbs)
+  while dom_map_changed:
+    dom_map_changed = False
+    # print(f'dom_map: {dom_map}', file=sys.stderr)
+    for bb in bbs[1:]:
+      new_doms = get_bb_doms(bb, bbs)
+      if new_doms != dom_map[bb[1]]:
+        dom_map[bb[1]] = new_doms
+        # print(f'Updated doms for {bb[1]}: {dom_map[bb[1]]}', file=sys.stderr)
+        dom_map_changed = True
+
+  tree = gen_d_tree(bbs)
+  # tree.display()
+
+  cfg = gen_cfg(bbs)
+  # cfg.display()
+  for bb in bbs:
+    frontier = dom_frontier(bb, cfg)
+    print(f'Dom Frontier for {bb[1]}: {frontier}', file=sys.stderr)
+
+  for d in defs:
+    for bb in dom_frontier(d, cfg):
+      print(f'Dom Frontier of {d}: {bb}', file=sys.stderr)
