202aef33405f8ab57ccb63b2fac782ac245077f1
Neel Patel <nmp83@cornell.edu>
2025-03-18 12:57:58 -0400

    simple example

--- lesson8/test.cpp
+++ lesson8/test.cpp
@@ -1,48 +1,21 @@
-// sample program that benefits from induction elimination
-#include <iostream>
-#include <x86intrin.h>
-#include <stdint.h>
-using namespace std;
-volatile int g_v = 0;
+#include <stdio.h>
 
-uint64_t func(){
-  uint64_t start, end;
+volatile int sink;  // Prevent removal of computed result
 
-  start = __rdtsc();
-  int x, y, z, w, a, b, c, d, e, f, g, h, j, k, l, m, n, o, p;
-  for (int i = 0; i < 10000000; ++i) {
-    // This loop is a simple induction variable
-    // that can be optimized by LICM.
-    x = 2 * i + 1;
-    y = 3 * i + 2;
-    z = 4 * i + 3;
-    w = 5 * i + 4;
-    a = 6 * i + 5;
-    b = 7 * i + 6;
-    c = 8 * i + 7;
-    d = 9 * i + 8;
-    e = 10 * i + 9;
-    f = 11 * i + 10;
-    g = 12 * i + 11;
-    h = 13 * i + 12;
-    j = 14 * i + 13;
-    k = 15 * i + 14;
-    l = 16 * i + 15;
-    m = 17 * i + 16;
-    n = 18 * i + 17;
-    o = 19 * i + 18;
-    p = 20 * i + 19;
-  }
-  g_v = x + y + z + w + a + b + c + d + e + f + g + h + j + k + l + m + n + o + p;
-  end = __rdtsc();
-  return end - start;
+void foo() {
+    int sum = 0;
+    // A simple loop with an induction variable 'i'
+    for (int i = 0; i < 1000; i++) {
+        // Compute an affine expression: 2*i + 1
+        int x = 2 * i + 1;
+        sum += x;
+    }
+    // Store the result in a volatile global variable.
+    sink = sum;
 }
 
 int main() {
-  int sum = 0;
-  int n = 1000;
-
-  uint64_t time = func();
-  cout << "Time: " << time << " cycles" << endl;
-  return 0;
+    foo();
+    printf("Sum: %d\n", sink);
+    return 0;
 }
