8e758600f062059c6417fcfcd8bf0e825b0a2694
Neel Patel <nmp83@cornell.edu>
2025-03-21 18:18:53 -0400

    remove bench

--- lesson8/benchmarks/compile_bench.sh
+++ lesson8/benchmarks/compile_bench.sh
@@ -4,8 +4,23 @@ CLANG=clang-18
 OPT=opt-18
 LLVM_AS=llvm-as-18
 POLYBENCH_DIR=$(pwd)/PolyBenchC-4.2.1
+INCLUDES="-I${POLYBENCH_DIR}/utilities -I${POLYBENCH_DIR}/linear-algebra/kernels/atax"
 
 PASS=$(pwd)/../build/licm/LICMPass.so
 
-# gcc -O3 -I utilities -I linear-algebra/kernels/atax utilities/polybench.c linear-algebra/kernels/atax/atax.c -DPOLYBENCH_TIME -o atax_time
-gcc -O3 -I ${POLYBENCH_DIR}/utilities -I ${POLYBENCH_DIR}/linear-algebra/kernels/atax ${POLYBENCH_DIR}/utilities/polybench.c ${POLYBENCH_DIR}/linear-algebra/kernels/atax/atax.c -DPOLYBENCH_TIME -o ${POLYBENCH_DIR}/linear-algebra/kernels/atax/atax_time
+# gcc -O3 -I ${POLYBENCH_DIR}/utilities -I ${POLYBENCH_DIR}/linear-algebra/kernels/atax ${POLYBENCH_DIR}/utilities/polybench.c ${POLYBENCH_DIR}/linear-algebra/kernels/atax/atax.c -DPOLYBENCH_TIME -o ${POLYBENCH_DIR}/linear-algebra/kernels/atax/atax_time
+
+${CLANG} -c ${POLYBENCH_DIR}/utilities/polybench.c -o ${POLYBENCH_DIR}/utilities/polybench.o
+
+
+${CLANG} -S -O1 -Xclang -disable-llvm-passes -emit-llvm ${INCLUDES} -S ${POLYBENCH_DIR}/linear-algebra/kernels/atax/atax.c -o ${POLYBENCH_DIR}/linear-algebra/kernels/atax/atax_time.ll
+
+# normal time
+${OPT} -passes=mem2reg -S ${POLYBENCH_DIR}/linear-algebra/kernels/atax/atax_time.ll -o ${POLYBENCH_DIR}/linear-algebra/kernels/atax/atax_time_mem2reg.ll
+${CLANG} -o ${POLYBENCH_DIR}/linear-algebra/kernels/atax/atax_time_mem2reg.o -c ${POLYBENCH_DIR}/linear-algebra/kernels/atax/atax_time_mem2reg.ll
+${CLANG} -o ${POLYBENCH_DIR}/linear-algebra/kernels/atax/atax_time_mem2reg ${POLYBENCH_DIR}/linear-algebra/kernels/atax/atax_time_mem2reg.o ${POLYBENCH_DIR}/utilities/polybench.o
+
+# licm time
+${OPT} -load-pass-plugin ${PASS} -passes=licm-invariant -S ${POLYBENCH_DIR}/linear-algebra/kernels/atax/atax_time_mem2reg.ll -o ${POLYBENCH_DIR}/linear-algebra/kernels/atax/atax_time_licm.ll
+${CLANG} -o ${POLYBENCH_DIR}/linear-algebra/kernels/atax/atax_time_licm.o -c ${POLYBENCH_DIR}/linear-algebra/kernels/atax/atax_time_licm.ll
+${CLANG} -o ${POLYBENCH_DIR}/linear-algebra/kernels/atax/atax_time_licm ${POLYBENCH_DIR}/linear-algebra/kernels/atax/atax_time_licm.o ${POLYBENCH_DIR}/utilities/polybench.o
