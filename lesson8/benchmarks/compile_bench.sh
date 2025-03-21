adbe8f758277fb2c5ef2ecdbf449a03da4297b2d
Neel Patel <neelpatelbiz@gmail.com>
2025-03-21 19:23:45 -0400

    include the flags

--- lesson8/benchmarks/compile_bench.sh
+++ lesson8/benchmarks/compile_bench.sh
@@ -5,15 +5,16 @@ OPT=opt-18
 LLVM_AS=llvm-as-18
 POLYBENCH_DIR=$(pwd)/PolyBenchC-4.2.1
 INCLUDES="-I${POLYBENCH_DIR}/utilities -I${POLYBENCH_DIR}/linear-algebra/kernels/atax"
+FLAGS="-DPOLYBENCH_TIME"
 
 PASS=$(pwd)/../build/licm/LICMPass.so
 
 # gcc -O3 -I ${POLYBENCH_DIR}/utilities -I ${POLYBENCH_DIR}/linear-algebra/kernels/atax ${POLYBENCH_DIR}/utilities/polybench.c ${POLYBENCH_DIR}/linear-algebra/kernels/atax/atax.c -DPOLYBENCH_TIME -o ${POLYBENCH_DIR}/linear-algebra/kernels/atax/atax_time
 
-${CLANG} -c ${POLYBENCH_DIR}/utilities/polybench.c -o ${POLYBENCH_DIR}/utilities/polybench.o
+${CLANG} ${FLAGS} -c ${POLYBENCH_DIR}/utilities/polybench.c -o ${POLYBENCH_DIR}/utilities/polybench.o
 
 
-${CLANG} -S -O1 -Xclang -disable-llvm-passes -emit-llvm ${INCLUDES} -S ${POLYBENCH_DIR}/linear-algebra/kernels/atax/atax.c -o ${POLYBENCH_DIR}/linear-algebra/kernels/atax/atax_time.ll
+${CLANG} ${FLAGS} -S -O1 -Xclang -disable-llvm-passes -emit-llvm ${INCLUDES} -S ${POLYBENCH_DIR}/linear-algebra/kernels/atax/atax.c -o ${POLYBENCH_DIR}/linear-algebra/kernels/atax/atax_time.ll
 
 # normal time
 ${OPT} -passes=mem2reg -S ${POLYBENCH_DIR}/linear-algebra/kernels/atax/atax_time.ll -o ${POLYBENCH_DIR}/linear-algebra/kernels/atax/atax_time_mem2reg.ll
