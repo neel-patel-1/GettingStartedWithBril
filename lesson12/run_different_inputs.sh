c33be81491500d13cd7cc472df1383fbbea4f3a6
Neel Patel <neelpatelbiz@gmail.com>
2025-04-25 21:55:50 -0400

    run different inputs

---
+++ lesson12/run_different_inputs.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+FNAME=$1
+FNAME=loopfact
+H_THRESH=2
+for arg in "$@"; do
+  if [[ $arg == "-h="* ]]; then
+    H_THRESH=${arg#-h=}
+    break
+  fi
+done
+INPUT=examples/${FNAME}.bril
+INPUT_JSON=examples/${FNAME}.json
+TRACE_DIR=./traces/${FNAME}.json
+OPT_TRACES=./opt/traces/${FNAME}.json/
+OUTPUT=opt/${FNAME}.json
+TRACE_BRILI=./trace-brili/brili.ts
+OG_BRILI=brili
+
+rm -rf $TRACE_DIR $OPT_TRACES $OUTPUT $INPUT_JSON guarded/traces/$FNAME.json
+cat $INPUT | bril2json > $INPUT_JSON
+# run through trace brili to generate the traces
+deno run --allow-read=./examples --allow-write=. $TRACE_BRILI $INPUT_JSON -h ${H_THRESH} 8
+echo "Before optimization: "
+echo "-----------------------"
+bril2txt < $INPUT_JSON
+echo "Output:"
+brili -p 8 < $INPUT_JSON
+
+mkdir -p $OPT_TRACES
+rm -rf filled/
+mkdir -p filled/$(basename $INPUT_JSON)
+no=0
+for trace in ${TRACE_DIR}/*; do
+  cp $trace $OPT_TRACES/$(basename $trace)
+  cp $trace ./trace_og_nofill
+  python3 optimizations/fill_labels.py $trace > filled/filled_${no}
+  cp -f filled/filled_${no} $trace
+  cp -f filled/filled_${no} $OPT_TRACES/$(basename $trace)
+  no=$((no + 1))
+done
+
+  # apply optimizations to the trace
+for trace in ${TRACE_DIR}/*; do
+  python3 ./optimizations/inline.py ${INPUT_JSON} ${trace} | python3 optimizations/lvn.py  -p -f > $OPT_TRACES/$(basename $trace)
+done
+
+# re-insert the traces
+echo "After optimization (Target Input): "
+echo "-----------------------"
+python3 ./optimizations/optimize_and_insert_trace.py $INPUT_JSON | python3 ../bril/examples/tdce.py tdce+ > $OUTPUT
+bril2txt < $OUTPUT
+echo "Output:"
+brili -p 8 < $OUTPUT
+
+echo "Before Optimization (Alternate Input): "
+echo "-----------------------"
+echo "Output:"
+brili -p 4 < $INPUT_JSON
+
+echo "After optimization (Alternate Input): "
+echo "-----------------------"
+python3 ./optimizations/optimize_and_insert_trace.py $INPUT_JSON | python3 ../bril/examples/tdce.py tdce+ > $OUTPUT
+bril2txt < $OUTPUT
+echo "Output:"
+brili -p 4 < $OUTPUT
